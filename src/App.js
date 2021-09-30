f014f5753f597f51ea708e9b83f4d5b05e6a809a
claricef <clariceoliver432@gmail.com>
2021-09-30 19:57:36 -0300

    requisições http

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './style.css';
+
+const App = () => {
+  const [nutri, setNutri] = useState([]);
+
+  useEffect(()=>{
+      function loadApi(){
+         let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+
+         fetch(url) // se essa requisição deu certo cai no then
+         .then((r)=> r.json()) // recebe o resultado (r) e transforma em json, se der certo vai no proximo then
+         .then((json)=>{
+              console.log(json);
+              setNutri(json);
+         })
+
+      }
+      loadApi();
+  },[]);
+
+  return( 
+    <div className='container' >
+        <header>
+          <stron>React Nutri</stron>
+        </header>
+
+        {nutri.map((item)=>{
+          return(
+            <article key={item.id} className="post">
+              <strong className="titulo">{item.titulo}</strong>
+              <img src={item.capa} alt={item.titulo} className="capa"/>
+              <p className="subtitulo">
+                {item.subtitulo}
+              </p>
+              <a className="botao"> Acessar</a>
+            </article>
+          )
+        })}
     </div>
-  );
+   );
 }
-
+ 
 export default App;
